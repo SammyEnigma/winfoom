b70235442846fdb8682267f30a7b3cc332b16f17
ecovaci <eugen.covaci.q@gmail.com>
2021-04-16 20:27:06 +0300

    Testing CI fail

--- src/test/java/org/kpax/winfoom/proxy/SocksProxyClientConnectionTests.java
+++ src/test/java/org/kpax/winfoom/proxy/SocksProxyClientConnectionTests.java
@@ -86,7 +86,7 @@ public class SocksProxyClientConnectionTests {
     @BeforeAll
     void before() throws Exception {
         beforeEach();
-        socksRemoteProxyServer = ClientAndServer.startClientAndServer("127.0.0.0", PROXY_PORT);
+        socksRemoteProxyServer = ClientAndServer.startClientAndServer("0.0.0.0", PROXY_PORT);
 
         remoteServer = ServerBootstrap.bootstrap().registerHandler("/get", new HttpRequestHandler() {
 
